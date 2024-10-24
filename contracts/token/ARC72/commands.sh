d28ec94ad01d5e2c082c488ef7bc5858e1eff0f7
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2024-10-24 00:09:43 -0600

    cleanup

--- contracts/token/ARC72/commands.sh
+++ contracts/token/ARC72/commands.sh
@@ -9,23 +9,12 @@ arc72-build-artifacts() {
 arc72-build-all() {
   arc72-build-image && arc72-build-artifacts
 }
-arc72-simulate() {
-  (
-    cd src
-    python simulate.py
-  )
-}
 arc72-cli() {
   (
     cd src/scripts
     source demo/utils.sh
     npx tsc
     cli ${@}
-  )
-}
-arc72-check-mab() {
-  (
-    bash check_mab.sh
   )
 }
 arc72-pytest() {
@@ -39,15 +28,6 @@ arc72-demo() {
     cd src/scripts
     npx tsc
     case ${1} in
-      "airdrop") {
-        bash demo/demo-contract-${1}.sh
-      } ;;
-      "staking") {
-        bash demo/demo-contract-${1}.sh
-      } ;;
-      "compensation") {
-        bash demo/demo-contract-${1}.sh
-      } ;;
       *) {
         echo "demo not found"
         false
@@ -66,34 +46,5 @@ arc72-mocha() {
     } || {
       npm run test-${1}
     }
-  )
-}
-arc72-plot() {
-  (
-    cd src
-    python plot-staking.py
-  )
-}
-arc72-program() {
-  (
-    set -e
-    cd src/scripts/program
-    npx tsc 
-    case ${1} in
-      airdrop|airdrop2|staking) {
-        node ${1}.js ${@:2}
-      } ;;
-      *) {
-        cat << EOF
-arc72-program
-
-  execute a program
-
-  USAGE scs-program airdrop [command] [args]
-
-EOF
-        false
-      } ;;
-    esac
   )
 }
