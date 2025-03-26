d96ec9267de25a9135db19db96ed9fc97e0addeb
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2025-03-25 18:38:34 -0600

    token arc72 initial

--- src/opensubmarine/__init__.py
+++ src/opensubmarine/__init__.py
@@ -8,16 +8,25 @@ from opensubmarine.contracts.token.ARC200.src.contract import (
     ARC200TokenInterface,
     arc200_Transfer,
 )
+from opensubmarine.contracts.token.ARC72.src.contract import (
+    ARC72Token,
+    ARC72TokenInterface,
+    arc72_Transfer,
+    arc72_nft_data,
+    arc72_holder_data,
+)
 from opensubmarine.contracts.utils.introspection.arc73.contract import (
     ARC73SupportsInterface,
 )
 
 
-__version__ = "0.1.19"
+__version__ = "0.2.2"
 
 __all__ = [
     "ARC200Token",
     "ARC200TokenInterface",
+    "ARC72Token",
+    "ARC72TokenInterface",
     "ARC73SupportsInterface",
     "BaseFactory",
     "Deployable",
@@ -27,6 +36,9 @@ __all__ = [
     "Stakeable",
     "Upgradeable",
     "arc200_Transfer",
+    "arc72_Transfer",
+    "arc72_nft_data",
+    "arc72_holder_data",
 ]
 
 OpenSubmarine_version = __version__
